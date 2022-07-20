5227d1fc57af055ce95c156fd7450f36e53eb319
Muslim Zamukaev <abu.safiia2016@gmail.com>
2022-07-20 17:20:44 +0200

    Update Events.tsx

--- src/component/Events/Events.tsx
+++ src/component/Events/Events.tsx
@@ -18,7 +18,7 @@ const Events: FC<EventsProps> = ({ events }) => {
 	const { searchValue } = useAppSelector(state => state.filter);
 	const filteredEvents = events.filter((event: IData) => event.title.toLowerCase().includes(searchValue.toLowerCase()));
 
-	if (filteredEvents[0]) {
+	if (filteredEvents[0].flyerFront) {
 		date = new Date(filteredEvents[0].date).toString().split(' ', 4).join(' ');
 	}
 
